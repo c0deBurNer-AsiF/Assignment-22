88d2ee6d15f6b0096f8316b4861aef0512edfef5
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-01-11 00:21:24 +0530

    Create qn-3.c

---
+++ qn-3.c
@@ -0,0 +1,30 @@
+/*3. Write a program to calculate the sum of n numbers entered by the user using malloc
+and free.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void input();
+int main()
+{
+    input();
+
+}
+void input()
+{
+    int n,*p,i;
+    printf("Enter how many numbers sum u want:\n");
+    scanf("%d",&n);
+    printf("Enter %d numbers:\n",n);
+    p=(int*)malloc(sizeof(int)*n);
+    for(i=0;i<n;i++)
+        scanf("%d",(p+i));
+
+    int sum=0;
+    for(i=0;i<n;i++)
+        sum=sum+p[i];
+
+    printf("sum of numbers %d",sum);
+
+
+}
