a1d6fb38efe11845367b3ba1ce6b9f43aeb3efc1
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-03-12 00:06:36 +0530

    Update qn-1.c

--- qn-1.c
+++ qn-1.c
@@ -1,5 +1,5 @@
 /*1. Define a function to input variable length string and store it in an array without
-memory wastage.*/
+memory wastage.
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -29,4 +29,43 @@ void display(char *s)
 {
     printf("%s",s);
     free(s);
+}*/
+
+
+/*1. Define a function to input variable length string and store it in an array without
+memory wastage.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+char* input();
+void display(char *);
+int main()
+{
+    char *p;
+    p=input();
+    display(p);
 }
+char* input()
+{
+    char *q,c;
+    printf("Enter the string:\n");
+    q=(char*)malloc(sizeof(char));
+
+    int i=0,j=1;
+    while(c!='\n')
+    {
+        c=getc(stdin);
+        j++;
+        q=(char*)realloc(q,j*sizeof(char));
+        q[i]=c;
+        i++;
+    }
+    q[i]='\0';
+    return q;
+}
+void display(char *r)
+{
+    printf("The string is: %s",r);
+    free(r);
+}
+
